ce57bbd0b77c4ce3d0be3741756b367a5479275e
Marcelo <marcelocjava@gmail.com>
2023-03-05 18:44:27 -0300

    Identação

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,19 @@
+import javax.lang.model.util.ElementScanner14;
+
+/**
+ * BoletimEstudantil
+ */
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+       
+        int mediaFinal = 7;
+
+            if (mediaFinal < 6 )
+                System.out.println("REPROVADO");
+            else if (mediaFinal == 6)
+                System.out.println("PROVA MINERVA");
+            else 
+                System.out.println("APROVADO");
+    }
+}
