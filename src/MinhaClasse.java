5b463b321fb24cdbb925ebba60b7efebd628f328
Marcelo <marcelocjava@gmail.com>
2023-03-05 18:28:46 -0300

    classes

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,7 +1,19 @@
 public class MinhaClasse {
   
     public static void main(String[] args) {
-        System.out.println("Olá turma, sejam bem vindos!");
+       System.out.println("Olá turma, sejam bem vindos!");
+
+        String primeiroNome = "Marcelo";
+        String segundoNome = "cruz";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(segundoNome);
+    
+}
 
 }
